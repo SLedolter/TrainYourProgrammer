4446d1ce2912bda981b35fb28bcc94218bcb2d83
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-11 13:20:15 +0200

    SL: solution added

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -3,7 +3,14 @@
 namespace TrainYourProgrammer {
   class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+      string raw_input;
+      int parsed_int;
+
+      do {
+        Console.Write("Bitte eine ganze Zahl eingeben: ");
+        raw_input = Console.ReadLine();
+      } while(!int.TryParse(raw_input, out parsed_int));
+      Console.WriteLine("Danke! Und tschüss...");
     }
   }
 }
