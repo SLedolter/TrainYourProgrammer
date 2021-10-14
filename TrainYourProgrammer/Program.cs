8bcf47b64ec2ffa45bcb747100128052938fdbf9
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-14 15:36:11 +0200

    SL: added prompting with little menu

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -2,8 +2,40 @@
 
 namespace TrainYourProgrammer {
   class Program {
+    const string c_prompt_label = "Eingabe: ";
+    const string c_commando_exit = "exit";
     static void Main(string[] args) {
+      string userInput;
 
+      while((userInput = ShowUserInputPrompt(c_prompt_label)).ToLower() != c_commando_exit) {
+        switch(userInput) {
+          case "calc":
+            ReadTwoNumbersAndMultiply();
+            break;
+
+          default:
+            Console.WriteLine("Kommando nicht gefunden!");
+            break;
+        }
+      }
+    }
+
+    static string ShowUserInputPrompt(string promptLabel) {
+      Console.Write(promptLabel);
+      return Console.ReadLine();
+    }
+
+    static void ReadTwoNumbersAndMultiply() {
+      string userInput;
+      int parsedInteger1, parsedInteger2;
+
+      while(!int.TryParse((userInput=ShowUserInputPrompt("First number: ")), out parsedInteger1)) {
+        Console.WriteLine("Only integer numbers!");
+      }
+      while(!int.TryParse((userInput = ShowUserInputPrompt("Second number: ")), out parsedInteger2)) {
+        Console.WriteLine("Only integer numbers!");
+      }
+      Console.WriteLine("To be done!");
     }
   }
 }
