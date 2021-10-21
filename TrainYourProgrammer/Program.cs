26a1a5c02f876f50d99e5ffab731ac91ae397e2f
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-21 14:23:19 +0200

    SL: added first draft of drawing the tree (not centered)

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -2,7 +2,7 @@
 
 namespace TrainYourProgrammer {
   class Program {
-    int blockHeight = 3;
+    static int blockHeight = 3;
 
     static void Main(string[] args) {
       string userInput;
@@ -13,7 +13,7 @@ namespace TrainYourProgrammer {
           Console.WriteLine("Nur Zahlen, bitte!");
         } else {
           if(treeHeight <= 3) {
-            Console.WriteLine("Babybäume dürfen erst ab einer Höhe von 3 verarbeitet werden!");
+            Console.WriteLine("Babybäume dürfen erst ab einer Höhe von 4 verarbeitet werden!");
           } else {
             CreateTreeInConsole(treeHeight);
             Console.ReadKey();
@@ -23,16 +23,21 @@ namespace TrainYourProgrammer {
       }
     }
 
-    static void CreateTreeInConsole(int wantedHeight) {
-      int possibleHeight = CalculateHeightDependingOnWindowSize(wantedHeight);
+    static void CreateTreeInConsole(int wantedTreeBlocks) {
+      int possibleHeight = CalculatePossibleHeightDependingOnWindowSize(wantedTreeBlocks);
       int possibleTreeBlocks = (possibleHeight - 1) / 3;
 
+      Console.WriteLine($"Fenstergröße: {Console.WindowWidth}x{Console.WindowHeight}, CursorTop: {Console.CursorTop}, Höhe: {wantedTreeBlocks}/{possibleTreeBlocks}, Benötigte Zeilen: {wantedTreeBlocks * 3 + 1}/{possibleHeight}");
+
+      for(int currentHeight = 1; currentHeight <= possibleTreeBlocks; currentHeight++) {
+        DrawSingleTreeBlock(currentHeight, 20);
+      }
     }
 
-    static int CalculateHeightDependingOnWindowSize(int blockCount) {
+    static int CalculatePossibleHeightDependingOnWindowSize(int blockCount) {
       int calculatedRows = blockCount * 3 + 1;
 
-      if(calculatedRows > Console.WindowHeight) {
+      while(calculatedRows > Console.WindowHeight) {
         calculatedRows -= 3;
       }
 
@@ -40,7 +45,12 @@ namespace TrainYourProgrammer {
     }
 
     static void DrawSingleTreeBlock(int blockNumber, int columnMidPoint) {
-
+      for(int i = blockNumber-1; i < blockNumber; i++) {
+        for(int row = 0; row < blockHeight; row++) {
+          string star = new string('*', (blockNumber-1)*2+(1+2*row));
+          Console.WriteLine(star);
+        }
+      }
     }
 
     static string GetUserInputWithPrompt(string prompt) {
