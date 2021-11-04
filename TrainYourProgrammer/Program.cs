e27ca7da576b4ab60d8778b1089af2883335b590
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-11-04 15:27:27 +0100

    SL: added unit tests

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -3,7 +3,13 @@
 namespace TrainYourProgrammer {
   class Program {
     static void Main(string[] args) {
-
+      Niederschlag niederschlag = new Niederschlag();
     }
+  }
+  
+  public class Niederschlag {
+    public int[] monatsWerte = new int[12];
+
+
   }
 }
---
+++ TrainYourProgrammerTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using TrainYourProgrammer;
+using Xunit;
+
+namespace TrainYourProgrammerTests {
+  public class UnitTest1 {
+    [Fact]
+    public void Test1() {
+      Niederschlag niederschlag = new Niederschlag(); 
+    }
+  }
+}
