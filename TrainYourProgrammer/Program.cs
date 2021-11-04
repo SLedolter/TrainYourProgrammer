e9453488eba08c9a59af40266ebf5386c5449f0e
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-11-04 14:01:11 +0100

    SL: enhanced calculation of midpoint for tree drawing

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -15,7 +15,7 @@ namespace TrainYourProgrammer {
           if(treeHeight <= 3) {
             Console.WriteLine("Babybäume dürfen erst ab einer Höhe von 4 verarbeitet werden!");
           } else {
-            CreateTreeInConsole(treeHeight);
+            DrawTreeInConsole(treeHeight);
             Console.ReadKey();
             Console.Clear();
           }
@@ -23,14 +23,29 @@ namespace TrainYourProgrammer {
       }
     }
 
-    static void CreateTreeInConsole(int wantedTreeBlocks) {
+    static void DrawTreeInConsole(int wantedTreeBlocks) {
       int possibleHeight = CalculatePossibleHeightDependingOnWindowSize(wantedTreeBlocks);
       int possibleTreeBlocks = (possibleHeight - 1) / 3;
+      int columnMidpoint = (int)Math.Ceiling(Decimal.Divide(CalculateMaximumTreeWidth(possibleTreeBlocks), 2));
 
       Console.WriteLine($"Fenstergröße: {Console.WindowWidth}x{Console.WindowHeight}, CursorTop: {Console.CursorTop}, Höhe: {wantedTreeBlocks}/{possibleTreeBlocks}, Benötigte Zeilen: {wantedTreeBlocks * 3 + 1}/{possibleHeight}");
 
       for(int currentHeight = 1; currentHeight <= possibleTreeBlocks; currentHeight++) {
-        DrawSingleTreeBlock(currentHeight, 20);
+        DrawSingleTreeBlock(currentHeight, columnMidpoint);
+      }
+      Console.SetCursorPosition(columnMidpoint - 3 / 2, Console.CursorTop);
+      Console.WriteLine("###");
+    }
+
+
+    static void DrawSingleTreeBlock(int blockNumber, int columnMidPoint) {
+      int oldConsoleTop = Console.CursorTop, oldConsoleLeft = Console.CursorLeft;
+      for(int i = blockNumber - 1; i < blockNumber; i++) {
+        for(int row = 0; row < blockHeight; row++) {
+          string star = new string('*', (blockNumber - 1) * 2 + (1 + 2 * row));
+          Console.SetCursorPosition(columnMidPoint - star.Length / 2, Console.CursorTop);
+          Console.WriteLine(star);
+        }
       }
     }
 
@@ -44,15 +59,12 @@ namespace TrainYourProgrammer {
       return calculatedRows;
     }
 
-    static void DrawSingleTreeBlock(int blockNumber, int columnMidPoint) {
-      int oldConsoleTop = Console.CursorTop, oldConsoleLeft = Console.CursorLeft;
-      for(int i = blockNumber-1; i < blockNumber; i++) {
-        for(int row = 0; row < blockHeight; row++) {
-          string star = new string('*', (blockNumber-1)*2+(1+2*row));
-          Console.SetCursorPosition(columnMidPoint - star.Length/2, Console.CursorTop);
-          Console.WriteLine(star);
-        }
-      }
+    static int CalculateMaximumTreeWidth(int blockCount) {
+      int result;
+
+      result = 1 + (blockCount - 1) * 2 + (blockHeight - 1) * 2;
+
+      return result;
     }
 
     static string GetUserInputWithPrompt(string prompt) {
