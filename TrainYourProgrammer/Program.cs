9bf1ebcf0a77b095c2302bf85586ae9496f10728
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-11-04 16:12:56 +0100

    SL: added tests for testing test

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -6,10 +6,21 @@ namespace TrainYourProgrammer {
       Niederschlag niederschlag = new Niederschlag();
     }
   }
-  
+
   public class Niederschlag {
     public int[] monatsWerte = new int[12];
 
+    public Niederschlag() {
+    }
 
+    public double DurchschnittBerechnen(int vonMonat, int bisMonat) {
+      int count = bisMonat - vonMonat;
+      int summe = 0;
+
+      for(int i = vonMonat-1; i<= bisMonat-1; i++) {
+        summe += monatsWerte[i];
+      }
+      return summe/count;
+    }
   }
 }
--- TrainYourProgrammerTests/UnitTest1.cs
+++ TrainYourProgrammerTests/UnitTest1.cs
@@ -4,8 +4,13 @@ using Xunit;
 namespace TrainYourProgrammerTests {
   public class UnitTest1 {
     [Fact]
-    public void Test1() {
-      Niederschlag niederschlag = new Niederschlag(); 
+    public void Test_Durchschnitt_1() {
+      Niederschlag niederschlag = new Niederschlag();
+
+      niederschlag.monatsWerte = new int[12] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+      Assert.Equal(1, niederschlag.DurchschnittBerechnen(1, 12));
     }
+
+
   }
 }
