ae748ec2db49c55f7fffbd528f8e4e9a0e2b4419
Stefan Ledolter <stefan.ledolter@ulrich.at>
2023-07-17 15:30:25 +0200

    SL: added solution

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -3,7 +3,22 @@
 namespace TrainYourProgrammer {
   class Program {
     static void Main(string[] args) {
+      int input;
+      int euro, cent;
+      string command = "";
 
+      do {
+        Console.WriteLine("Geben Sie die Cent ein: ");
+        command = Console.ReadLine();
+
+        if(int.TryParse(command,out _)) {
+          input = int.Parse(command);
+          euro = input / 100;
+          cent = input - (euro * 100);
+          Console.WriteLine($"Das ergibt {euro} Euro und {cent} Cent.\n");
+        }
+
+      } while (command != "exit");
     }
   }
 }
