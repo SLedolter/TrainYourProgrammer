ca42802197763bf7a61d66f2315cde6aa02850fe
Stefan Ledolter <stefan.ledolter@ulrich.at>
2023-07-18 11:37:40 +0200

    SL: added solutino

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using ConsoleLibrary;
 
 namespace TrainYourProgrammer {
   class Program {
     static void Main(string[] args) {
+      int startValue, endValue;
 
+      startValue = Console2k.ReadInt("Startwert: ");
+      endValue = Console2k.ReadInt("Endwert: ");
+
+      Console.WriteLine();
+      DisplayValues(startValue, endValue);
+    }
+
+    static void DisplayValues(int start, int end) {
+      for(int i = start; i <= end; i++) {
+        Console.WriteLine(i);
+      }
     }
   }
 }
