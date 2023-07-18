213e5de374d881f69644e21f959e2426a4d2003a
Stefan Ledolter <stefan.ledolter@ulrich.at>
2023-07-18 12:57:42 +0200

    SL: added solution

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -5,7 +5,14 @@ using ConsoleLibrary;
 namespace TrainYourProgrammer {
   class Program {
     static void Main(string[] args) {
+      string input;
+      Console.WriteLine("Geben Sie ein Wort ein:");
+      input = Console.ReadLine();
+      Console.WriteLine();
 
+      for(int i = input.Length; i > 0; i--) {
+        Console.WriteLine(input);
+      }
     }
   }
 }
