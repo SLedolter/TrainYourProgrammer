d1818020bbe444108df7ff5fdab979bf3533002b
Stefan Ledolter <stefan.ledolter@ulrich.at>
2023-08-30 11:57:20 +0200

    SL: made blank code again

--- TrainYourProgrammer/Program.cs
+++ TrainYourProgrammer/Program.cs
@@ -5,24 +5,7 @@ using ConsoleLibrary;
 namespace TrainYourProgrammer {
   class Program {
     static void Main(string[] args) {
-      Niederschlag niederschlag = new Niederschlag();
-    }
-  }
 
-  public class Niederschlag {
-    public int[] monatsWerte = new int[12];
-
-    public Niederschlag() {
-    }
-
-    public double DurchschnittBerechnen(int vonMonat, int bisMonat) {
-      int count = bisMonat - vonMonat;
-      int summe = 0;
-
-      for(int i = vonMonat-1; i<= bisMonat-1; i++) {
-        summe += monatsWerte[i];
-      }
-      return summe/count;
     }
   }
 }
